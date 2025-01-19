b4336d948400875925a82a39bba8cc6384263d62
walterlijunior <114302150+walterlijunior@users.noreply.github.com>
2025-01-19 09:04:16 -0300

    Update translation.py
    Código que irá traduzir

---
+++ backend/services/translation.py
@@ -0,0 +1,6 @@
+from googletrans import Translator
+
+def translate_text(text, target_language='en'):
+    translator = Translator()
+    translation = translator.translate(text, dest=target_language)
+    return translation.text
